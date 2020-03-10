7e0b6f652c1137f3ba61efed5518da30dd5fe366
bls <blair.strang@auspost.com.au>
2020-03-10 17:26:40 +1100

    Add SSH issuer. Make tests run.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/bsycorp/keymaster
 
 go 1.13
+
+require (
+	github.com/jonboulle/clockwork v0.1.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.5.1
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+)
---
+++ keymaster/creds/ssh/ssh.go
@@ -0,0 +1,102 @@
+package ssh
+
+import (
+	"bytes"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"github.com/jonboulle/clockwork"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+	"io"
+)
+
+const (
+	KeyBits            = 2048
+	MaxValidForSeconds = 7 * 24 * 3600
+)
+
+type User struct {
+	Identity        string
+	Principals      []string
+	ValidForSeconds int
+}
+
+type Creds struct {
+	Certificate []byte
+	PrivateKey  []byte
+}
+
+type Issuer struct {
+	Random io.Reader
+	Clock  clockwork.Clock
+}
+
+func (issuer *Issuer) GenerateKeyPair(user *User) (ssh.PublicKey, *rsa.PrivateKey, error) {
+	if user.ValidForSeconds < 0 || user.ValidForSeconds > MaxValidForSeconds {
+		return nil, nil, errors.New("Invalid issuance period")
+	}
+	if issuer.Random == nil {
+		return nil, nil, errors.New("No random source? what happened?")
+	}
+
+	// Generate user private key
+	log.Println("Generating RSA private key")
+	privateKey, err := rsa.GenerateKey(issuer.Random, KeyBits)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Generate user public key
+	log.Println("Getting public key")
+	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return publicKey, privateKey, nil
+}
+
+func (issuer *Issuer) CreateSignedCertificate(ca ssh.Signer, publicKey ssh.PublicKey, privateKey *rsa.PrivateKey, user *User, extensions map[string]string, options map[string]string) (*Creds, error) {
+	// Create a signed SSH certificate for the user
+	// As per: https://www.ietf.org/mail-archive/web/secsh/current/msg00327.html
+	now := uint64(issuer.Clock.Now().Unix())
+	userCert := &ssh.Certificate{
+		CertType:        ssh.UserCert,
+		KeyId:           user.Identity,
+		ValidPrincipals: user.Principals,
+		ValidAfter:      now,
+		ValidBefore:     now + uint64(user.ValidForSeconds),
+		Key:             publicKey,
+		Permissions: ssh.Permissions{
+			CriticalOptions: options,
+			Extensions: extensions,
+		},
+	}
+	// Sign the user's key with the CA key
+	log.Println("Signing SSH certificate")
+	if err := userCert.SignCert(issuer.Random, ca); err != nil {
+		return nil, err
+	}
+
+	// Marshal the user's certificate
+	log.Println("Marshalling SSH certificate")
+	userCertBytes := ssh.MarshalAuthorizedKey(userCert)
+
+	// Marshal the user's private key
+	log.Println("Marshalling SSH private key")
+	privateKeyPEM := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}
+	var private bytes.Buffer
+	if err := pem.Encode(&private, privateKeyPEM); err != nil {
+		return nil, err
+	}
+
+	sshCreds := Creds{
+		Certificate: userCertBytes,
+		PrivateKey:  private.Bytes(),
+	}
+
+	log.Println("Successfully issued SSH credentials")
+	return &sshCreds, nil
+}
