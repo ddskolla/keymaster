106b65e4c5db66e663f1410ad87e117db31ad791
bls <blair.strang@auspost.com.au>
2020-03-25 10:18:23 +1100

    Skeleton workflow.

--- km/workflow/workflow.go
+++ km/workflow/workflow.go
@@ -1,2 +1,45 @@
 package workflow
 
+import "context"
+
+type Workflow struct {
+
+}
+
+type StartRequest struct {
+
+}
+
+type StartResponse struct {
+
+}
+
+type GetDetailsRequest struct {
+
+}
+
+type GetDetailsResponse struct {
+	
+}
+
+type GetAssertionsRequest struct {
+
+}
+
+type GetAssertionsResponse struct {
+
+}
+
+func (w *Workflow) Start(ctx context.Context, req *StartRequest) (*StartResponse, error) {
+	// HTTP
+	return nil, nil
+}
+
+func (w *Workflow) GetDetails(ctx context.Context, req *GetDetailsRequest) (*GetDetailsResponse, error) {
+	// HTTP
+	return nil, nil
+}
+
+func (w *Workflow) GetAssertions(ctx context.Context, req *GetAssertionsRequest) (*GetAssertionsResponse, error) {
+	return nil, nil
+}
